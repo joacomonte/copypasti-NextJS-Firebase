b24ab4fd23cec6cbb212c9fe508e10821947d72c
Joaco <montesano1994@gmail.com>
2023-05-25 18:52:42 -0300

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,74 @@
-import Image from 'next/image'
+
+import { revalidatePath } from 'next/cache';
 import styles from './page.module.css'
 
+
+
+
+interface iCopyPasty {
+  code: number;
+  text: string;
+}
+
+function generateRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const copyPastyList: iCopyPasty[] = []
+
 export default function Home() {
+
+async function saveNewCopyPasty(data: FormData){
+  "use server";
+  const newCode = generateRandomNumber(1000, 9999);
+  const copyPasty = data.get("textToCopy") as string;
+  
+  copyPastyList.push({
+    text: copyPasty,
+    code: newCode
+  });
+  revalidatePath("/");
+  
+}
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section className={styles.title}>
+        <h1>Copy-Pasty
+        </h1>
+      </section>
+      <section className={styles.card}>
+
+        <form action={saveNewCopyPasty}>
+          <p>Text</p>
+          <input 
+            type='text-area'
+            name='textToCopy'
+          />
+          <button type='submit'>Generate Code</button>
+        </form>
+
+        <form>
+          <p>Code</p>
+          <input name='codeToGet' type='number'/>
+          <button type='submit'>Get text</button>
+        </form>
+
+        <div style={{margin: "50px"}}>
+        {copyPastyList.length > 0 && (
+          <div>
+            <ul>
+              {copyPastyList.length > 0 && (
+                <>
+                <h4>Your code is: </h4>
+                <p>Code: {copyPastyList[copyPastyList.length - 1].code}</p>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   )
 }
